9b2e398a91863516edf8280da105098832ac3c4c
JakubF0 <jfurtak954@gmail.com>
2026-01-17 17:58:54 +0000

    add activity4

--- Activity4.py
+++ Activity4.py
@@ -1,6 +1,7 @@
+#Gather column titles
 col1 = input("Enter column 1 title: ")
 col2 = input("Enter column 2 title: ")
 col3 = input("Enter column 3 title: ")
-
+# output labeled table
 print(f"{col1:<20}{col2:<20}{col3:<20}")
 print("-" * 60)
