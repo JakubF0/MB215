2f8f83bb0a7cbca29d5b5450019c26952b5d8422
JakubF0 <jfurtak954@gmail.com>
2026-01-17 18:25:35 +0000

    add activity6

--- Activity6.py
+++ Activity6.py
@@ -1,10 +1,13 @@
+# get two values from the user
 a = float(input("Enter value for a: "))
 b = float(input("Enter value for b: "))
 
+# perform calculations
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 
+# zero check
 if b != 0:
     print("Division:", a / b)
     print("Integer Division:", a // b)
