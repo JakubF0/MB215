48e4212745eb85969cb255521681375ede64dad5
JakubF0 <jfurtak954@gmail.com>
2026-01-17 17:34:16 +0000

    add Activity1.py

---
+++ Activity1.py
@@ -0,0 +1,10 @@
+# details of user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Calculations for birth year
+current_year = 2026
+birth_year = current_year - age
+
+# displayed results
+print(f"Hello {name}! You were born in {birth_year}.")
