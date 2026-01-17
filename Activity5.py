666c8fabe622c7a626457871e57414e6b014a310
JakubF0 <jfurtak954@gmail.com>
2026-01-17 18:17:56 +0000

    add activity5

---
+++ Activity5.py
@@ -0,0 +1,9 @@
+# Define constatn (Pi)
+PI = 3.14159
+
+# Calculate are using constant and input
+radius = float(input("Enter radius: "))
+area = PI * radius ** 2
+
+# output area
+print(f"Area of circle: {area}")
